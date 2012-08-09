b5c4b026eb8afa3d5abcb1be16c3f6bf65b48ee0
Paul Mezzanini <paul@themezz.com>
2012-08-09 15:10:01 -0400

    used bananas instead of backticks and removed old script

--- submit-generation.sh
+++ submit-generation.sh
@@ -41,7 +41,7 @@ STEP_WORKER_SCRIPT="generation-step.sh"
 #The steps inside them
 
 
-for GENERATION in `seq 0 ${TOTAL_GENERATIONS}` ; do
+for GENERATION in $(seq 0 ${TOTAL_GENERATIONS}) ; do
 
 cat << EOF >> ${JOB_FILE_DIRECTORY}/${JOB_NAME}_${GENERATION}.sh
 #!/bin/bash -l
@@ -87,8 +87,8 @@ if [ "$?" != "0" ] ; then
 else
 EOF
 
-for STEPNUMBER in `seq 0 ${NUM_STEPS}` ; do
-	echo srun -c 1 ${STEP_WORKER_SCRIPT} ${GENERATION} ${STEPNUMBER}
+for STEPNUMBER in $(seq 0 $(expr ${NUM_STEPS} - 1) ) ; do
+	echo srun -c 1 ${STEP_WORKER_SCRIPT} ${GENERATION} ${STEPNUMBER} >> ${JOB_FILE_DIRECTORY}/${JOB_NAME}_${GENERATION}.sh
 done
 
 echo "fi" >> ${JOB_FILE_DIRECTORY}/${JOB_NAME}_${GENERATION}.sh
@@ -96,55 +96,3 @@ echo "fi" >> ${JOB_FILE_DIRECTORY}/${JOB_NAME}_${GENERATION}.sh
 
 done
 
-#Old script down there...
-exit 0
-
-
-mkdir -p ${dataPath}output/
-dep_list='';
-for particle in $(seq 0 $(expr $num_particles_per_gen - 1)) ; do
-   outfile=${dataPath}output/output-generation-0-particle-$particle.out
-   job=$jobname-0-$particle
-   generation=0
-   echo "Submitting job $job"
-   export particle
-   export generation
-   command="sbatch \
-	--qos=salvaggio-normal \
-	-J $job \
-	--output=$outfile \
-	--error=$outfile \
-	$jobfile"
-   id=$($command | awk ' { print $4 }')
-   echo $id
-   dep_list=$id:$dep_list
-   echo "latest list $dep_list"
-done
-echo $dep_list
-# Now submit the remaining jobs so that they depend on their predecessors.
-for i in `seq 1 $num_generations` ; do
-    dep_list=$(echo ${dep_list%\:})
-    echo $dep_list
-    dep_list_next=''
-    for particle in $(seq 0 $(expr $num_particles_per_gen - 1)) ; do
-        # Do a little output to see what's up as this executes.
-        echo "Submitting my particle $particle in generation ${i} using dep_list $dep_list."
-
-        # Name a file dynamically where we want all of our 'messages' to go.
-        outfile=${dataPath}output/output-generation-$i-particle-$particle.out
-	generation=$i
-	export generation
-	export particle
-	
-	echo "current dep list $dep_list"
-	command="sbatch \
-		--qos=salvaggio-normal \
-		--dependency=afterok:$dep_list \
-		-J $jobname-$i-$particle \
-		--output=$outfile \
-		--error=$outfile \
-		$jobfile"
-	dep_list_next=$($command | awk ' { print $4 }'):$dep_list_next
-    done
-    dep_list=$dep_list_next
-done
