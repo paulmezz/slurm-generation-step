759fb1590fc46ef12b7f8619af3c430216a9666a
Paul Mezzanini <paul@themezz.com>
2012-08-14 14:25:09 -0400

    more meaningful info

--- generation-step.sh
+++ generation-step.sh
@@ -4,7 +4,6 @@
 MY_GENERATION=$1
 MY_THREAD=$2
 
-echo "I am worker ${MY_THREAD} of generation ${MY_GENERATION}"
-echo "\"working\" for 10 seconds"
+echo "worker ${MY_THREAD} of generation ${MY_GENERATION} reporting in from node $(hostname)"
 sleep 10
 
